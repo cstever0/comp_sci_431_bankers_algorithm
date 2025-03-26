b7273d627266198f3e1f2be9ee004afd25ae0166
Cory Stever <cstever@hotmail.com>
2025-03-26 15:15:53 -0500

    Print Statements added for better demonstration

--- bankers_algorithm.py
+++ bankers_algorithm.py
@@ -1,15 +1,18 @@
 import numpy as np
 
 # Function to calculate the need matrix
-def calculate_need_matrix(allocate, max_need):
-    return max_need - allocate
+def calculate_need_matrix(allocation, max_need):
+    return max_need - allocation
 
 
 # Function to check if the system is in a safe state or not - Safety Algorithm
-def safety_algorithm(processes, allocate, need, available):
-    work = available.copy()
+def safety_algorithm(processes, allocation, need, available):
+    resources = available.copy()
     complete = np.full(len(processes), False, dtype=bool)
     safe_sequence = []
+
+    print(f"Available resources before beginning process execution: {resources} \n")
+    print("------------ Attempting Process Execution ------------------ \n")
 
     # Loop through processes until all processes have been flagged as complete
     while not np.all(complete):
@@ -18,12 +21,17 @@ def safety_algorithm(processes, allocate, need, available):
         # Check if processes can be allocated resources
         for i in range(len(processes)):
             # Check if process is complete and the resources needed is less than the resources available
-            if not complete[i] and np.all(need[i] <= work):
-                work += allocate[i] # release resources previously allocated to the process
+            if not complete[i] and np.all(need[i] <= resources):
+                # Print statement to demonstrate resources available before process execution.
+                print(f"Process {processes[i]} is executing. Resources available before allocation: {resources}")
+
+                resources += allocation[i] # release resources previously allocated to the process
                 complete[i] = True # flag the process as complete
-                safe_sequence.append(processes[i]) # add the process to the safe_sequence of processes
+                safe_sequence.append(str(processes[i])) # add the process to the safe_sequence of processes
                 found = True # set found flag to true
-                break
+
+                # Print statement to demonstrate resources available after process completion.
+                print(f"Process {processes[i]} has completed execution. Updated resources available: {resources} \n")
 
         # Process that can be completed was not found and system is not in a safe state
         if not found:
@@ -34,19 +42,19 @@ def safety_algorithm(processes, allocate, need, available):
 
 
 # Main Function of the Banker's Algorithm
-def bankers_algorithm(processes, allocate, max_need, available):
+def bankers_algorithm(processes, allocation, max_need, available):
     # calculate the need matrix
-    need = calculate_need_matrix(allocate, max_need)
-    return safety_algorithm(processes, allocate, need, available)
+    need = calculate_need_matrix(allocation, max_need)
+    return safety_algorithm(processes, allocation, need, available)
 
 
-# A function that can dynamically check processes requesting resources and allocate available resources if safe
-def resource_request_algorithm(processes, allocate, max_need, available, process_index, resource_request):
+# A function that can dynamically check processes requesting resources and allocation available resources if safe
+def resource_request_algorithm(processes, allocation, max_need, available, process_idx, resource_request):
     # calculate the need matrix
-    need = calculate_need_matrix(allocate, max_need)
+    need = calculate_need_matrix(allocation, max_need)
 
     # Step 1: check if requested resources exceeds the max resources needed for that process
-    if np.any(resource_request > need[process_index]):
+    if np.any(resource_request > need[process_idx]):
         return False # resource request exceeds max need for the process
 
     # Step 2: Check if there are resources available to complete the resource request
@@ -54,30 +62,30 @@ def resource_request_algorithm(processes, allocate, max_need, available, process
         return False # resource request exceeds available resources
 
     # Step 3: Resources are available and the resource request can be allocated
-    temp_allocate = allocate.copy()
+    temp_allocation = allocation.copy()
     temp_available = available.copy()
 
-    # allocate resources to the requesting process
-    temp_allocate[process_index] += resource_request
+    # allocation resources to the requesting process
+    temp_allocation[process_idx] += resource_request
     temp_available -= resource_request
 
     # recalculate the need matrix
-    temp_need = calculate_need_matrix(temp_allocate, max_need)
+    temp_need = calculate_need_matrix(temp_allocation, max_need)
 
     # Step 4: Check if the system is in a safe state after allocation
-    is_safe, sequence = safety_algorithm(processes, temp_allocate, temp_need, temp_available)
+    is_safe, sequence = safety_algorithm(processes, temp_allocation, temp_need, temp_available)
 
     return is_safe
 
 
 # Data Set given from assignment to test functionality
 processes = np.array(["P0", "P1", "P2", "P3", "P4"])
-allocate = np.array([[0, 1, 0], [2, 0, 0], [3, 0, 2], [2, 1, 1], [0, 0, 2]])
+allocation = np.array([[0, 1, 0], [2, 0, 0], [3, 0, 2], [2, 1, 1], [0, 0, 2]])
 max_need = np.array([[7, 5, 3], [3, 2, 2], [9, 0, 2], [2, 2, 2], [4, 3, 3]])
 available = np.array([3, 3, 2])
 
 # Demonstrating functionality of Banker's Algorithm
-is_safe, safe_sequence = bankers_algorithm(processes, allocate, max_need, available)
+is_safe, safe_sequence = bankers_algorithm(processes, allocation, max_need, available)
 if is_safe:
     print(f"The system completed all processes and remained in a Safe State. The Safe Sequence of processes was: {safe_sequence}")
 else:
